549c19cf894242e49cf9d775f9e6aee151410067
Katrin Kircheva <katrin.kircheva@gmail.com>
2026-03-27 10:23:51 +0200

    initial implementation

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,14 +1,12 @@
 // Firebase Configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDrQ9cBzb0aC4pxJ8yK23vxLp-DdbvmtsU",
-  authDomain: "no-waste-tracker.firebaseapp.com",
-  databaseURL: "https://no-waste-tracker-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "no-waste-tracker",
-  storageBucket: "no-waste-tracker.firebasestorage.app",
-  messagingSenderId: "786373283923",
-  appId: "1:786373283923:web:644bc4b586ba0fa51f36a6",
-  measurementId: "G-FWJ46PLHY1"
+  apiKey: "AIzaSyCPypWGpYGGN9_dWrKX1D6IABobKJeZ8RM",
+  authDomain: "no-waste-tracker-f05be.firebaseapp.com",
+  projectId: "no-waste-tracker-f05be",
+  storageBucket: "no-waste-tracker-f05be.firebasestorage.app",
+  messagingSenderId: "466422657232",
+  appId: "1:466422657232:web:13d8df0d1cd30da4d06ed5"
 };
 
 // Initialize Firebase
